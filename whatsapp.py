2d8045ccb3ac5b77f454f17620be27a8e4ef2183
Ratnakar <ratnakar3895@yahoo.com>
2021-03-29 21:47:06 +0530

    version 1.2

--- whatsapp.py
+++ whatsapp.py
@@ -10,11 +10,11 @@ driver.get("https://web.whatsapp.com/")
 input("Please scan QR code and press any key to continue")
 
 
-receiver = driver.find_element_by_css_selector('span[title="Amit jio"]')
-receiver.click()
-
-textinput = driver.find_element_by_xpath(r"/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]")
 while True:
+    name = input("Enter the receiver's name properly and it is case sensitive: ")
+    receiver = driver.find_element_by_css_selector(f'span[title="{name}"]')
+    receiver.click()
+    textinput = driver.find_element_by_xpath(r"/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]")
     n = int(input("Enter the number of times you want to send the message: "))
     message = input("Enter the message: ")
     delay = float(input("Enter the minutes after which you want to send the message: "))
