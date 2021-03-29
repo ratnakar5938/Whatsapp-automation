2f270b29aae410c7e198872d58475d7e08020c3b
Ratnakar <ratnakar3895@yahoo.com>
2021-03-29 21:21:03 +0530

    Version 1.1

--- whatsapp.py
+++ whatsapp.py
@@ -14,7 +14,14 @@ receiver = driver.find_element_by_css_selector('span[title="Amit jio"]')
 receiver.click()
 
 textinput = driver.find_element_by_xpath(r"/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]")
-time.sleep(15) # this will delay the sending
-for i in range(20):
-    textinput.send_keys("Hemlo domst")
-    textinput.send_keys(Keys.RETURN)
+while True:
+    n = int(input("Enter the number of times you want to send the message: "))
+    message = input("Enter the message: ")
+    delay = float(input("Enter the minutes after which you want to send the message: "))
+    time.sleep(delay*60) # this will delay the sending
+    for i in range(n):
+        textinput.send_keys(message)
+        textinput.send_keys(Keys.RETURN)
+    end = input("Enter exit to exit the program else just press enter: ")
+    if end == "exit":
+        break
